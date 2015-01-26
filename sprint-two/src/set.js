e9efe0664b4d88bd74db87337869b568acdb2eb0
plauer <plauer411@gmail.com>
2015-01-25 20:34:49 -0800

    Add set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item)
 };
 
 setPrototype.contains = function(item){
+  if (this._storage.indexOf(item) !== -1) {
+    return true
+  }
+  return false
 };
 
 setPrototype.remove = function(item){
+  var index = this._storage.indexOf(item)
+  this._storage.splice(index, 1)
 };
 
 /*
