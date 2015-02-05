f6c366b76961e75fdd2fe0c48d1ba51ac7588235
plauer <plauer411@gmail.com>
2015-02-05 15:11:34 -0800

    Clean up linked list.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -7,7 +7,7 @@ var makeLinkedList = function(){
     var node = makeNode(value);
     if (this.tail === null && this.head === null) {
       this.tail = this.head = node;
-      return
+      return;
     }
 
     this.tail.next = node;
@@ -15,21 +15,21 @@ var makeLinkedList = function(){
   };
 
   list.removeHead = function(){
-    var oldHead = this.head.value
+    var oldHead = this.head.value;
     this.head = this.head.next;
-    return oldHead
+    return oldHead;
   };
 
   list.contains = function(target){
     var recurseMe = function (node) {
       if (node.value === target) return true;
       if (node.next !== null) {
-        return recurseMe(node.next)
+        return recurseMe(node.next);
       }
-      return false
-    }
+      return false;
+    };
 
-    return recurseMe(this.head)
+    return recurseMe(this.head);
   };
 
   return list;
@@ -44,27 +44,9 @@ var makeNode = function(value){
   return node;
 };
 
-var linkedList = makeLinkedList();
-linkedList.addToTail(4);
-linkedList.addToTail(7);
-linkedList.addToTail(5);
-console.log(linkedList.contains(4))
-console.log(linkedList.contains(5))
-console.log(linkedList.contains(7))
-console.log(linkedList.contains(6))
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-// A linkedList class, in functional style, with the following properties:
-// .head property, a linkedListNode instance
-// .tail property, a linkedListNode instance
-// .addToTail() method, takes a value and adds it to the end of the list
-// .removeHead() method, removes the first node from the list and returns its value
-// .contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
-// What is the time complexity of the above functions?
 
 
 
