1aeaa7ea80bc32052eb941c11123bfddb531aaf6
plauer <plauer411@gmail.com>
2015-02-04 21:51:51 -0800

    Refactor functional-shared stack to hold data in private variables.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,7 +1,7 @@
 var makeStack = function() {
   var stack = _.extend({}, stackMethods);
-  stack.storage = {};
-  stack.currentSize = 0;
+  stack._storage = {};
+  stack._currentSize = 0;
   // _.extend(stack, stackMethods);
 
   return stack;
@@ -9,22 +9,22 @@ var makeStack = function() {
 
 var stackMethods = {
   'size' : function() {
-    return this.currentSize;
-    // return Object.keys(this.storage).length;
+    return this._currentSize;
+    // return Object.keys(this._storage).length;
   },
   'push' : function(item) {
-    this.currentSize++;
-    this.storage[this.currentSize] = item;
+    this._currentSize++;
+    this._storage[this._currentSize] = item;
   },
   'pop' : function() {
-    var result = this.storage[this.currentSize];
-    delete this.storage[this.currentSize];
-    this.currentSize = this.currentSize - 1 < 0 ? 0 : this.currentSize - 1;
+    var result = this._storage[this._currentSize];
+    delete this._storage[this._currentSize];
+    this._currentSize = this._currentSize - 1 < 0 ? 0 : this._currentSize - 1;
 
     return result;
   }
 };
 console.log('first run');
 var stack = makeStack();
-console.log(stack.currentSize);
+console.log(stack._currentSize);
 console.log('end');
